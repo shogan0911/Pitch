9e314fee77f76d6b801f28257c8beeebb777967a
scteh <sctehogan@gmail.com>
2022-10-04 13:24:05 -0700

    pair 1.1

--- Pair Trading_new.py
+++ Pair Trading_new.py
@@ -24,6 +24,7 @@ for i in range(len(grouped_by_naics_code)):
     if len(grouped_by_naics_code.iloc[i]) >= 4:
         company_by_naics_code.append(x)
 
+
 #%%
 
 ticker_data_dict = {}
@@ -45,40 +46,27 @@ for q in range(len(company_by_naics_code)):
             daily_close_by_ticker.append(daily_close)
 
         ticker_data_dict[company_by_naics_code[q][e]] = daily_close_by_ticker
+        final[q] = daily_close_by_ticker
         daily_close_by_ticker = []
-
-    final[q] = ticker_data_dict
-    ticker_data_dict = {}
     print(q)
-
 #%%
 
-len(final[2]['PR'])
-
-#%%
-
-ticker_data_dict = {}
-daily_close_by_ticker = []
-price_data = []
-final = {}
-
+e = 0
+q = 0
 
 for q in range(len(company_by_naics_code)):
 
     for e in range(len(company_by_naics_code[q])):
 
-        ticker = company_by_naics_code[q][e]
-        stock_price_data = requests.get(
-            'https://api.polygon.io/v2/aggs/ticker/' + ticker + '/range/1/day/2020-09-28/2022-09-30?adjusted=true&sort=asc&apiKey=x4kfbN2sNWrGtNGbEQT8ORSlLfxoSkx3').json()
-
-        for i in range(len(stock_price_data['results'])):
-            daily_close = stock_price_data['results'][i]['c']
-            daily_close_by_ticker.append(daily_close)
-
-        ticker_data_dict[company_by_naics_code[q][e]] = daily_close_by_ticker
-        daily_close_by_ticker = []
+        ticker_ph = company_by_naics_code[q][e]
+        max = max(ticker_data_dict[q][ticker_ph])
+        print(max)
 
 #%%
+ticker_data_dict[0]['AR']
+
+
+
 
 
 #%%
